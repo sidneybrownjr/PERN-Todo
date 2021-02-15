9208d3c2ffe4666364772644b6bd0273bcdadb7d
Sid <46009984+sidneybrownjr@users.noreply.github.com>
2021-02-15 15:35:57 -0600

    db

--- db.js
+++ db.js
@@ -1,14 +1,6 @@
 const Pool = require('pg').Pool;
 require('dotenv').config();
 
-/* const devConfig = {
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-}; */
-
 const devConfig = `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
 
 //heroku
@@ -17,6 +9,9 @@ const proConfig = process.env.DATABASE_URL;
 const pool = new Pool({
   connectionString:
     process.env.NODE_ENV === 'production' ? proConfig : devConfig,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
